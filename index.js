c60bd3e64cadddbb7aac761863b1147942fdb2d9
Jesse <jessemerge2@gmail.com>
2022-12-12 11:05:40 -0800

    Initialize EJS and file path

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-import express from 'express';
+import express, { request } from 'express';
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    console.log('home')
+app.get('/', (req, res) => {
+    res.render('pages/home');
 });
+
+app.get('/list', (req, res) => {
+    res.render('pages/list');
+});
+
+app.get('/detail', (req, res) => {
+    res.render('pages/detail');
+});
+
+app.use(function (req, res) {
+    res.status(404).render('pages/404', { query: req.url });
+})
+
 
 app.listen(1994);
