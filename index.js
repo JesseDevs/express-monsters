d368a3539b17fda5e7d17d1f5cf250d7960a545e
Jesse <jessemerge2@gmail.com>
2022-12-12 15:20:42 -0800

    Use data monsters to create list and display

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import fileSystem from 'node:fs';
+import fileSystem, { appendFileSync } from 'node:fs';
+import express from 'express';
+import pokesData from './data/pokemon-data.json' assert {type: 'json'}
 
 fileSystem.readdir('./css', function (err, files) {
     if (err) {
@@ -25,18 +27,20 @@ fileSystem.readdir('./css', function (err, files) {
     console.log("Done :)")
 })
 
-import express from 'express';
+
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.static('css'));
+
+app.locals.pokemons = pokesData;
 
 app.get('/', (req, res) => {
     res.render('pages/home');
 });
 
 app.get('/list', (req, res) => {
-    res.render('pages/list');
+    res.render('pages/list', { pokemons: pokesData });
 });
 
 app.get('/detail', (req, res) => {
