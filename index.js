40607032e897f4930b38bb68efe11a2574b42cb7
Jesse <jessemerge2@gmail.com>
2022-12-14 19:39:06 -0800

    Add scss functioninality

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
-import fileSystem, { appendFileSync } from 'node:fs';
+import fileSystem from 'node:fs';
 import express from 'express';
 import pokesData from './data/pokemon-data.json' assert {type: 'json'}
-
-fileSystem.readdir('./css', function (err, files) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-
-    const cssFiles = files.filter(file => file.endsWith('.css'));
-    let totalContent = '';
-    const destination = 'compiled.css';
-
-    cssFiles.forEach(function (file) {
-
-        var fileContents = fileSystem.readFileSync(`./css/${file}`, 'utf8');
-        if (file == 'compiled.css') {
-            var fileContents = ''
-        }
-
-        totalContent += fileContents;
-    })
-
-    fileSystem.writeFileSync(`./css/${destination}`, totalContent, function (err) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-    })
-    console.log("Done :)")
-})
 
 
 const app = express();
@@ -38,22 +9,24 @@ app.use(express.static('public'));
 app.use(express.static('css'));
 
 
+app.listen(1994, function () {
+    console.log("Server starting... localhost:1994")
+});
+
 app.get('/', (req, res) => {
     res.render('pages/home');
 });
 
-app.get('/pokes', (req, res) => {
-    res.render('pages/pokes', { pokemons: pokesData });
+app.get('/pokemons', (req, res) => {
+    res.render('pages/pokemons', { pokemons: pokesData });
 });
 
-app.get('/pokes/:id', (req, res) => {
-    let pokemon = '';
-    pokesData.forEach(poke => {
-        if (poke.id == req.params.id) {
-            pokemon = poke;
-        }
-    });
-    res.render('pages/poke', { pokemon });
+app.get('/pokemon/:id', (req, res) => {
+
+    const pokemon = pokesData.find(function (pokemon) {
+        return pokemon.id == req.params.id
+    })
+    res.render('pages/pokemon', { pokemon });
 });
 
 
@@ -61,4 +34,3 @@ app.use(function (req, res) {
     res.status(404).render('pages/404', { query: req.url })
 })
 
-app.listen(1994);
